90a77cb0ce76df18db739e0c5e17991654bc580f
devahsan22 <ahsandevcodes@gmail.com>
2019-03-20 09:15:00 -0500

    Implement User model with password hashing

--- models/User.js
+++ models/User.js
@@ -4,57 +4,24 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true, 'Username is required'],
+        required: true,
         unique: true,
-        trim: true,
-        minlength: [3, 'Username must be at least 3 characters long'],
-        maxlength: [30, 'Username cannot exceed 30 characters']
+        trim: true
     },
     email: {
         type: String,
-        required: [true, 'Email is required'],
+        required: true,
         unique: true,
-        lowercase: true,
-        trim: true,
-        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+        trim: true
     },
     password: {
         type: String,
-        required: [true, 'Password is required'],
-        minlength: [6, 'Password must be at least 6 characters long']
+        required: true,
+        minlength: 6
     },
-    firstName: {
-        type: String,
-        trim: true,
-        maxlength: [50, 'First name cannot exceed 50 characters']
-    },
-    lastName: {
-        type: String,
-        trim: true,
-        maxlength: [50, 'Last name cannot exceed 50 characters']
-    },
-    avatar: {
-        type: String,
-        default: null
-    },
-    isActive: {
-        type: Boolean,
-        default: true
-    },
-    lastLogin: {
+    createdAt: {
         type: Date,
-        default: null
-    },
-    preferences: {
-        theme: {
-            type: String,
-            enum: ['light', 'dark'],
-            default: 'light'
-        },
-        notifications: {
-            type: Boolean,
-            default: true
-        }
+        default: Date.now
     }
 }, {
     timestamps: true,
@@ -71,12 +38,12 @@ userSchema.virtual('fullName').get(function() {
 userSchema.index({ email: 1 });
 userSchema.index({ username: 1 });
 
-// Pre-save middleware to hash password
+// Hash password before saving
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     
     try {
-        const salt = await bcrypt.genSalt(12);
+        const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
         next();
     } catch (error) {
@@ -84,7 +51,7 @@ userSchema.pre('save', async function(next) {
     }
 });
 
-// Method to compare password
+// Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
     return bcrypt.compare(candidatePassword, this.password);
 };
