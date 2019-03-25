dac0cdf3576f40bf1b1f0b263c93835e44f87d70
devahsan22 <ahsandevcodes@gmail.com>
2019-03-25 13:40:00 -0500

    Implement basic authentication routes (login/register)

--- routes/auth.js
+++ routes/auth.js
@@ -51,113 +51,61 @@ const generateToken = (userId) => {
   );
 };
 
-// @route   POST /api/auth/register
-// @desc    Register a new user
-// @access  Public
-router.post('/register', validateRegistration, async (req, res) => {
+// Register user
+router.post('/register', async (req, res) => {
   try {
-    // Check for validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        success: false,
-        errors: errors.array()
-      });
-    }
+    const { username, email, password } = req.body;
 
-    const { username, email, password, firstName, lastName } = req.body;
-
-    // Check if user already exists
-    const existingUser = await User.findOne({
-      $or: [{ email }, { username }]
-    });
-
+    // Check if user exists
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
     if (existingUser) {
-      return res.status(400).json({
-        success: false,
-        message: existingUser.email === email 
-          ? 'Email already registered' 
-          : 'Username already taken'
-      });
+      return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Create new user
-    const user = new User({
-      username,
-      email,
-      password,
-      firstName,
-      lastName
-    });
-
+    // Create user
+    const user = new User({ username, email, password });
     await user.save();
 
-    // Generate token
     const token = generateToken(user._id);
 
-    // Update last login
-    user.lastLogin = new Date();
-    await user.save();
-
     res.status(201).json({
-      success: true,
-      message: 'User registered successfully',
-      data: {
-        user: user.getPublicProfile(),
-        token
-      }
+      user: { id: user._id, username: user.username, email: user.email },
+      token
     });
 
   } catch (error) {
     console.error('Registration error:', error);
-    res.status(500).json({
-      success: false,
-      message: 'Server error during registration'
-    });
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
-// @route   POST /api/auth/login
-// @desc    Login user
-// @access  Public
-router.post('/login', validateLogin, async (req, res) => {
+// Login user
+router.post('/login', async (req, res) => {
   try {
-    // Check for validation errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        success: false,
-        errors: errors.array()
-      });
-    }
-
     const { email, password } = req.body;
 
-    // Find user by credentials
-    const user = await User.findByCredentials(email, password);
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
 
-    // Generate token
+    // Check password
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
     const token = generateToken(user._id);
 
-    // Update last login
-    user.lastLogin = new Date();
-    await user.save();
-
     res.json({
-      success: true,
-      message: 'Login successful',
-      data: {
-        user: user.getPublicProfile(),
-        token
-      }
+      user: { id: user._id, username: user.username, email: user.email },
+      token
     });
 
   } catch (error) {
     console.error('Login error:', error);
-    res.status(401).json({
-      success: false,
-      message: 'Invalid credentials'
-    });
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
