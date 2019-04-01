2cf650591038365aa782021ec7ce054e6f37d78e
devahsan22 <ahsandevcodes@gmail.com>
2019-04-01 08:30:00 -0500

    Connect routes to server and complete basic API setup

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const authRoutes = require('./routes/auth');
+const todosRoutes = require('./routes/todos');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +20,10 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todo-app'
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/todos', todosRoutes);
 
 // Basic routes
 app.get('/api/health', (req, res) => {
