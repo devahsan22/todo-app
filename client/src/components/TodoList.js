67a0dfe4603aa92386d970fdd09de1dd9a3197ac
devahsan22 <ahsandevcodes@gmail.com>
2019-04-08 14:15:00 -0500

    Integrate frontend with backend API for todo operations

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,29 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TodoList = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Build todo app', completed: false },
-    { id: 3, text: 'Deploy to production', completed: false }
-  ]);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(true);
 
-  const addTodo = () => {
-    if (newTodo.trim()) {
-      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
-      setNewTodo('');
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('/api/todos', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setTodos(data);
+      }
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const toggleTodo = (id) => {
-    setTodos(todos.map(todo => 
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    ));
+  const addTodo = async () => {
+    if (newTodo.trim()) {
+      try {
+        const response = await fetch('/api/todos', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          },
+          body: JSON.stringify({ title: newTodo })
+        });
+        
+        if (response.ok) {
+          const todo = await response.json();
+          setTodos([todo, ...todos]);
+          setNewTodo('');
+        }
+      } catch (error) {
+        console.error('Error adding todo:', error);
+      }
+    }
   };
 
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+  const toggleTodo = async (id) => {
+    try {
+      const todo = todos.find(t => t._id === id);
+      const response = await fetch(`/api/todos/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify({ completed: !todo.completed })
+      });
+      
+      if (response.ok) {
+        const updatedTodo = await response.json();
+        setTodos(todos.map(todo => 
+          todo._id === id ? updatedTodo : todo
+        ));
+      }
+    } catch (error) {
+      console.error('Error updating todo:', error);
+    }
   };
+
+  const deleteTodo = async (id) => {
+    try {
+      const response = await fetch(`/api/todos/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      
+      if (response.ok) {
+        setTodos(todos.filter(todo => todo._id !== id));
+      }
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+    }
+  };
+
+  if (loading) {
+    return <div>Loading todos...</div>;
+  }
 
   return (
     <div className="todo-list">
@@ -42,14 +111,14 @@ const TodoList = () => {
 
       <ul>
         {todos.map(todo => (
-          <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+          <li key={todo._id} className={todo.completed ? 'completed' : ''}>
             <input
               type="checkbox"
               checked={todo.completed}
-              onChange={() => toggleTodo(todo.id)}
+              onChange={() => toggleTodo(todo._id)}
             />
-            <span>{todo.text}</span>
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            <span>{todo.title}</span>
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
           </li>
         ))}
       </ul>
