c0a3e4a41599af1ae9572aae52ba8d9729ef5d2c
devahsan22 <ahsandevcodes@gmail.com>
2019-04-10 11:30:00 -0500

    Add user authentication with login form and session management

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (userData) => {
+    setUser(userData);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todo App</h1>
         <p>Manage your tasks efficiently</p>
+        {user && (
+          <div className="user-info">
+            <span>Welcome, {user.username}!</span>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        )}
       </header>
       <main>
-        <TodoList />
+        {user ? <TodoList /> : <Login onLogin={handleLogin} />}
       </main>
     </div>
   );
---
+++ client/src/components/Login.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        onLogin(data.user);
+      } else {
+        alert('Login failed. Please check your credentials.');
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+      alert('Login failed. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" disabled={loading}>
+          {loading ? 'Logging in...' : 'Login'}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login; 
