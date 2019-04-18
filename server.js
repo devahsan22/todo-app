472236f60e0d57b0fbe856d3e804df02a386d429
devahsan22 <ahsandevcodes@gmail.com>
2019-04-18 15:40:00 -0500

    Add error handling middleware and 404 route handler

--- server.js
+++ server.js
@@ -30,6 +30,17 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running' });
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+// 404 handler
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
